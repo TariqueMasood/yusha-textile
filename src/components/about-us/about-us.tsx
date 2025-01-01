4adc60fd4c15e72436f6644d64d4e018c630ba88
TariqueMasood <64158034+TariqueMasood@users.noreply.github.com>
2025-01-01 12:59:18 +0530

    CSE: Title added

--- src/components/about-us/about-us.tsx
+++ src/components/about-us/about-us.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import styles from "./about-us.module.css";
 
 export const AboutUs = () => {
   return (
     <div className="container">
-      <h1>About Us</h1>
+      <h1 className={styles.title}>
+        Our website is currently under development. Please check back soon for
+        updates!
+      </h1>
     </div>
   );
 };
